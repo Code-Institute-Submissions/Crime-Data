7427a934f50ce6030dae878ec6bc22c98a2b0255
shanehowley <snhowley@gmail.com>
2017-03-24 15:40:35 +0000

    maybe this time

--- Crime_Data.py
+++ Crime_Data.py
@@ -8,16 +8,21 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # code for running locally
-# MONGODB_HOST = 'localhost'
-# MONGODB_PORT = 27017
-# DBS_NAME = 'Crime_Data'
-# COLLECTION_NAME = 'new_data'
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'Crime_Data'
+COLLECTION_NAME = 'new_data'
 # code for running locally
 
 # code for running on heroku
-MONGODB_URI = os.getenv('MONGODB_URI')
-DBS_NAME = os.getenv('MONGO_DB_NAME', 'Crime_Data')
-COLLECTION_NAME = os.getenv('MONGO_COLLECTION_NAME', 'Crime_Data')
+# MONGODB_URI = os.getenv('MONGODB_URI')
+# DBS_NAME = os.getenv('MONGO_DB_NAME', 'Crime_Data')
+# COLLECTION_NAME = os.getenv('MONGO_COLLECTION_NAME', 'Crime_Data')
+
+
+MONGODB_URI = 'mongodb://heroku_69fn47cd:i9lqct7bi6jv94mbk4dfi4j20@ds137550.mlab.com:37550/heroku_69fn47cd'
+DBS_NAME = 'heroku_69fn47cd'
+COLLECTION_NAME = 'new_data'
 
 # code for running on heroku
 
@@ -45,7 +50,7 @@ def crime_new_data():
               'County': True,
               '_id': False}
 
-    with MongoClient(MONGODB_URI) as conn:
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
         collection = conn[DBS_NAME][COLLECTION_NAME]
 
         new_data = collection.find(projection=FIELDS)
@@ -58,3 +63,9 @@ if __name__ == "__main__":
 
 # for getting database on m lab MongoDB
 # mongoimport -h ds135820.mlab.com:35820 -d heroku_m8nfsm78 -c new_data -u heroku_m8nfsm78 -p eq55vmr1br17alr2djut1joke3 --file garda_stations.csv --type csv --headerline
+
+
+
+# mongoimport -h ds137550.mlab.com:37550 -d heroku_69fn47cd -c new_data -u heroku_69fn47cd -p i9lqct7bi6jv94mbk4dfi4j20 --file Crime_Data_new_data_498887.json
+#
+# mongodb://heroku_69fn47cd:i9lqct7bi6jv94mbk4dfi4j20@ds137550.mlab.com:37550/heroku_69fn47cd
